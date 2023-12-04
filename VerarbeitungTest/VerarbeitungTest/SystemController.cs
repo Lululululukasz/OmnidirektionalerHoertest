624074260bc3b00da1e27252679c452aeef1f16b
Veronica <veronica.zylla@student.fh-kiel.de>
2023-12-05 00:59:37 +0100

    wip

--- VerarbeitungTest/VerarbeitungTest/SystemController.cs
+++ VerarbeitungTest/VerarbeitungTest/SystemController.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
+using static VerarbeitungTest.OscRouter;
 
 namespace VerarbeitungTest
 {
     internal class SystemController
     {
+        double calibrationOffset;
+        TestController testController;
+        SoundDomeView soundDomeView;
+
+        void startTest()
+        {
+            testController = new TestController(calibrationOffset, soundDomeView);
+        }
+
+        public SystemController()
+        {
+            OscRouter router = new OscRouter();
+            router.AddReceiver((message) =>
+            {
+                if (message == "click:1") cancelTest();
+            }, SubscriberType.System);
+        }
     }
 }
