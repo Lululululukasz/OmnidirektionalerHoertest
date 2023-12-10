eecbaec398ee6c46f6eed8187e76a3de5c2b43ee
Robert <elrotoro8@gmail.com>
2023-12-10 15:12:28 +0100

    branch init

--- VerarbeitungTest/VerarbeitungTest/SoundDomeView.cs
+++ VerarbeitungTest/VerarbeitungTest/SoundDomeView.cs
@@ -8,6 +8,18 @@ namespace VerarbeitungTest
 {
     internal class SoundDomeView
     {
+        public enum FeedbackType
+        {
+            rise,
+            fall,
+            beep
+        }
+
+
+        private static Object _lock;
+        private static Thread soundThread;
+        private List<Question> QuestionQueue;
+        private List<FeedbackType> FeedbackQueue;
         public void askQuestion(Question question)
         {
 
