778764e20b60496885249ade6d9d703f68dbf637
Robert <elrotoro8@gmail.com>
2023-12-11 21:49:28 +0100

    Callibrationcontroller hinzugefügt

--- VerarbeitungTest/VerarbeitungTest/TestController.cs
+++ VerarbeitungTest/VerarbeitungTest/TestController.cs
@@ -9,16 +9,16 @@ namespace VerarbeitungTest
 {
     internal class TestController
     {
-        private double calibrationOffset;
-        private QuestionController questionController;
-        private Action<Question> soundDomeView;
-        private Test test;
-        private OscRouter router;
-        private bool testStarted;
-        private long inputTimer = 0;
-        private bool callibrationRunning;
-        private double callibrationBuffer;
-        private int callibrations = 0;
+        protected double calibrationOffset;
+        protected QuestionController questionController;
+        protected Action<Question> soundDomeView;
+        protected Test test;
+        protected OscRouter router;
+        protected bool testStarted;
+        protected long inputTimer = 0;
+        protected bool callibrationRunning;
+        protected double callibrationBuffer;
+        protected int callibrations = 0;
         public TestController(Action<Question> viewCallback, OscRouter router, double callibration)
         {
             this.router = router;
@@ -91,65 +91,8 @@ namespace VerarbeitungTest
                 
             }
         }
-        public double startCallibration()
-        {
-            double cal = 0;
-            router.AddReceiver(routerCallibrationCallback, OscRouter.SubscriberType.Test);
-            callibrationBuffer = 0;
-            
-            Question q = new Question();
-            q.angle = 0;
-            q.pitch = 200;
-            questionController.generateQuestion(q);
-            questionController.askQuestion();
-            callibrationRunning = true;
-            long timeoutTimer = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
-            while (callibrationRunning && (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond) - timeoutTimer < 30000)//timeout for callibration if no inputs happen
-            {
-
-            }
-            cal = callibrationBuffer / 4;
-            callibrationRunning = false;
-            return cal;
-        }
-        public void routerCallibrationCallback(string data)
-        {
-            if (callibrationRunning && (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond) - inputTimer > 1000)// Limit imput to once every second
-            {
-                inputTimer = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
-                double answer = -1;
-                data = data.Replace(',', '.');//regio code formatting
-                string inputType = data.Split(':')[0];
-                string answerstr = data.Split(":")[1];
-                if (inputType.CompareTo("alpha") == 0)
-                {
-                    try
-                    {
-                        answer = double.Parse(answerstr);
-                    }
-                    catch (Exception e)
-                    {
-
-                    }
-                }
-                double offset = Math.Abs(answer - questionController.getCurrentQuestion().angle);
-                callibrationBuffer += offset;
-                callibrations++;
-                if(callibrations > 4)
-                {
-                    callibrationRunning = false;
-                }
-                else
-                {
-                    Question q = questionController.getCurrentQuestion();
-                    q.angle += 90;
-                    questionController.generateQuestion(q);
-                    questionController.askQuestion();
-                    Thread.Sleep(500);
-                }
-                
-            }
-        }
+        
+        
         public bool isTestFinished()
         {
             return !testStarted;
