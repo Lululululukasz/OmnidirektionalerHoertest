9373af5bc86353dbcf2f0d37deb1f16c8976b6ce
Helene Luening <helene.i.n.luening@student.fh-kiel.de>
2023-12-11 21:04:13 +0100

    Osc reciever, osc router and question controller Tests refactored

--- VerarbeitungTest/VerarbeitungTest/Tests/OscReceiver.Test.cs
+++ VerarbeitungTest/VerarbeitungTest/Tests/OscReceiver.Test.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using Rug.Osc;
 
 
@@ -12,34 +13,19 @@ namespace VerarbeitungTest
     /// <summary>
     /// Test Class for the OscReceiver Class. It Starts Automatically after Creation if the Test is Passed it shoud show "Test -> OscReceiver : OK"
     /// </summary>
+
+    [TestFixture]
+
     internal class OscReceiverTest
     {
-        private OscRouter dummy;
-        private OscReceiver receiver;
         private int receivedDebugPacks = 0;
         private int receivedJibberish = 0;
-        private bool testDone = false;
+
         private OscSender sender;
-        public OscReceiverTest()
+
+        private void Callback(string data)
         {
-            receiver = new OscReceiver(ReceiveTest,9000);
-            IPAddress ip = IPAddress.Parse("127.0.0.1");
-            int port = 9000;
-            sender = new OscSender(ip, port);
-            sender.Connect();
-            for(int i = 0;i < 10; i++)
-            {
-                sender.Send(new OscMessage("/I1-2/mouse/OSCTESTDATA", 42));
-            }
-            
-        }
-        /// <summary>
-        /// Test Callback for the OscReceiver Class
-        /// </summary>
-        public void ReceiveTest(string data)
-        {
-            //Console.WriteLine(data);
-            if(data.CompareTo("/I1-2/mouse/OSCTESTDATA, 42") == 0)
+            if (data.CompareTo("/I1-2/mouse/OSCTESTDATA, 42") == 0)
             {
                 receivedDebugPacks++;
             }
@@ -47,27 +33,27 @@ namespace VerarbeitungTest
             {
                 receivedJibberish++;
             }
-            if(receivedDebugPacks >= 10 && receivedJibberish == 0) {
-                Console.WriteLine("Test -> OscReceiver : OK");
-                receiver.Close();
-                sender.Close();
-                testDone = true;
-
-            }
-            else if(receivedJibberish > 10 )
-            {
-                Console.WriteLine("Test -> OscReceiver : FAILED");
-                receiver.Close();
-                sender.Close();
-                testDone = true;
-            }
         }
-        public bool testComplete()
+
+        [Test]
+        public void OscReceiveTest()
         {
-            return testDone;
+            OscReceiver receiver = new OscReceiver(Callback, 9000);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 9000;
+            sender = new OscSender(ip, port);
+            sender.Connect();
+            for (int i = 0; i < 10; i++)
+            {
+                sender.Send(new OscMessage("/I1-2/mouse/OSCTESTDATA", 42));
+            }
+
+            Thread.Sleep(1000);
+
+            Assert.Multiple(() => {
+                Assert.That(receivedDebugPacks, Is.EqualTo(10));
+                Assert.That(receivedJibberish, Is.Zero);
+            });
         }
-
-
-        
     }
 }
